efb6961ac27f82cfde5101544e4a1109563c6d23
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2022-11-23 23:52:22 +0100

    Remove useless exception ctor

--- src/main/java/com/github/antoinejt/exassert/exceptions/ExAssertInternalException.java
+++ src/main/java/com/github/antoinejt/exassert/exceptions/ExAssertInternalException.java
@@ -1,6 +1,7 @@
 package com.github.antoinejt.exassert.exceptions;
 
 public class ExAssertInternalException extends RuntimeException {
+
   public ExAssertInternalException(final String msg) {
     super(msg);
   }
--- src/main/java/com/github/antoinejt/exassert/exceptions/NumberSignException.java
+++ src/main/java/com/github/antoinejt/exassert/exceptions/NumberSignException.java
@@ -1,9 +1,6 @@
 package com.github.antoinejt.exassert.exceptions;
 
 public class NumberSignException extends AssertionFailedException {
-  public NumberSignException() {
-    super();
-  }
 
   public NumberSignException(final String msg) {
     super(msg);
